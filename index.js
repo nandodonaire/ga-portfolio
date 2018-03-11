688d3d196e66356c4888ec0e7eee7f750a39e896
nandodonaire <nandodonaire@gmail.com>
2018-03-11 17:04:05 -0400

    Added background images to 'about-me' and 'resume'
    --Added background images to 'about-me' and 'resume' and added a bit
    more spacing between sections.

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 $(() => {
+  // $(document).ready(function () {
+  //   $('.parallax').parallax()
+  // })
+  $(document).ready(function () {
+    $('.button-collapse').sideNav()
+  })
   // fuctions to add scroll animation when clickin on nav bar buttons.
   $('.about-me-scroll').click(function () {
     $('html, body').animate({
