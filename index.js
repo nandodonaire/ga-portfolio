dd7203b9c02890a1b4d383597a7bc9307d6ff2b8
nandodonaire <nandodonaire@gmail.com>
2018-03-11 14:19:34 -0400

    Added scroll animation to nav bar
    --Added animation when a user clicks on the links in the nav bar.

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 $(() => {
-
+  // fuctions to add scroll animation when clickin on nav bar buttons.
+  $('.about-me-scroll').click(function () {
+    $('html, body').animate({
+      scrollTop: $('.about-me').offset().top
+    }, 1000)
+  })
+  $('.skills-scroll').click(function () {
+    $('html, body').animate({
+      scrollTop: $('.skills').offset().top
+    }, 1000)
+  })
+  $('.resume-scroll').click(function () {
+    $('html, body').animate({
+      scrollTop: $('.resume').offset().top
+    }, 1000)
+  })
+  $('.projects-scroll').click(function () {
+    $('html, body').animate({
+      scrollTop: $('.projects').offset().top
+    }, 1000)
+  })
+  $('.contact-me-scroll').click(function () {
+    $('html, body').animate({
+      scrollTop: $('.page-footer').offset().top
+    }, 1000)
+  })
 })
